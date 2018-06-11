83bb6a032e6dac5f2c343c82d5214237a5ed7487
mihkel26 <mihkel260@gmail.com>
2018-06-11 14:29:14 +0300

    7 day avg price at every hour

--- main/python/src/prices.py
+++ main/python/src/prices.py
@@ -3,36 +3,42 @@ import urllib.request
 import urllib.error
 import datetime
 
-
-
 url = 'https://dashboard.elering.ee/et/api/nps?type=price&start=startdate+21%3A00%3A00&end=enddate+21%3A00%3A00&format=csv'
 data = []
-
+avg = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+       0.0, 0.0]
 
 today = datetime.date.today()
 startdate = (datetime.datetime.now() - datetime.timedelta(days=7)).date()
 
-url = "https://dashboard.elering.ee/et/api/nps?type=price&start=" + str(startdate) + "+21%3A00%3A00&end=" + str(today) +"+21%3A00%3A00&format=csv"
+url = "https://dashboard.elering.ee/et/api/nps?type=price&start=" + str(startdate) + "+21%3A00%3A00&end=" + str(
+    today) + "+21%3A00%3A00&format=csv"
 
 
 def getPrices():
-    #Download electricity market price
+    # Download electricity market price
     try:
         urllib.request.urlretrieve(url, 'output.csv')
     except urllib.error.HTTPError as ex:
         print('Problem:', ex)
 
-    #Read data from file
+    # Read data from file
     with open('output.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         for row in reader:
             if row[0] == 'ee':
+                data.append(
+                    [int(datetime.datetime.fromtimestamp(int(row[1])).strftime('%H')), float(row[2].replace(",", "."))])
 
-                data.append([int(datetime.datetime.fromtimestamp(int(row[1])).strftime('%H')), float(row[2].replace(",", "."))])
+    for i in range(len(data)):
+        # print(data[i][1])
+        for j in range(24):
+            if data[i][0] == j:
+                avg[j] += data[i][1]
 
-    return(print(data))
+    for el in range(len(avg)):
+        avg[el] = avg[el] / 7
 
+    return (avg)
 
-
-
-getPrices()
+print(getPrices())
