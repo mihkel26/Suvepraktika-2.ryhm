31aea9feb41dc398efdb3e9283aecabd8c4bfe6c
mihkel26 <mihkel260@gmail.com>
2018-06-12 15:34:36 +0300

    calculates price

--- main/python/src/calcPrice.py
+++ main/python/src/calcPrice.py
@@ -2,12 +2,18 @@ import prices
 import powerUsage
 import datetime
 
+
 now = datetime.datetime.now()
 
 
-def price_market():
+def price_market(start_hour, end_hour):
+    time = end_hour - start_hour
     today_price = prices.getTodayPrice()
-    price = today_price[now.hour] * powerUsage.get_mwh(1)
+    price = 0
+    for i in range(time):
+        price += today_price[start_hour+i] * powerUsage.get_mwh()
     return price
+
+print(price_market(12, 16))
 
 
--- main/python/src/powerUsage.py
+++ main/python/src/powerUsage.py
@@ -7,6 +7,7 @@ def get_kw():
     return kw
 
 
-def get_mwh(time):
+def get_mwh():
+    time = 1
     mwh = get_kw() * time / 1000
     return mwh
