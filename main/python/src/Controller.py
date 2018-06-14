fde8193b316c52560981dc3ac9f1feb31feffae5
mihkel26 <mihkel260@gmail.com>
2018-06-14 10:20:20 +0300

    Controller check state

--- main/python/src/Controller.py
+++ main/python/src/Controller.py
@@ -15,3 +15,11 @@ def get_room_temp():
 
 def get_dev_temp():
     return subprocess.check_output(["sudo", "python", "main.py", "1", "600", "3", "/dev/ttyUSB0"])
+
+
+def check_state():
+    state = subprocess.check_output(["sudo", "python", "main.py", "1", "0", "1", "/dev/ttyUSB0"])
+    if state == 0.0:
+        return "OFF"
+    else:
+        return "ON"
