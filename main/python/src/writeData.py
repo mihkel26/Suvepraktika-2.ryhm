2403598eb2721f100e355d76f43d90badbf9aa56
mihkel26 <mihkel260@gmail.com>
2018-06-14 11:21:04 +0300

    write data to file

--- main/python/src/writeData.py
+++ main/python/src/writeData.py
@@ -2,15 +2,15 @@ import Controller
 
 
 def file_write():
-
     file = open("data.txt", "w")
 
-    file.write(Controller.check_state())
+    file.write(Controller.check_state() + "\n")
     file.write(Controller.get_dev_temp())
     file.write(Controller.get_room_temp())
 
     file.close()
 
     return
+
 
 file_write()
