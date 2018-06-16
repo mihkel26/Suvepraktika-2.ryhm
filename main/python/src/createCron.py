cc006f5d0238ecd36971b7eca9a8f5a3bc20d1f4
mihkel26 <mihkel260@gmail.com>
2018-06-16 09:01:52 +0300

    Timedelta

--- main/python/src/createCron.py
+++ main/python/src/createCron.py
@@ -1,9 +1,8 @@
-on_hours = "*"
-on_minutes = "*"
-on_day = "*"
-on_month = "*"
+import datetime
+
 off_day = "*"
 off_month = "*"
+year = datetime.date.today().year
 
 f = open('userNeeds.txt')
 lines = f.readlines()
@@ -11,23 +10,20 @@ off_hours, off_minutes = map(int, (lines[2].split(".")))
 off_day, off_month = map(int, (lines[3].split(".")))
 on_day, on_month = map(int, (lines[3].split(".")))
 
-warming_time = "00.10"
+warming_time = "1.45"
 warming_h, warming_m = map(int, (warming_time.split(".")))
 
-print(warming_h)
-print(warming_m)
-
-on_hours = off_hours - warming_h
-# fix with timedelta
-if off_minutes == 00:
-    on_minutes = 60 - warming_m
+off = datetime.datetime(year, off_month, off_day, off_hours, off_minutes)
+print(off)
+on = off - datetime.timedelta(hours=warming_h, minutes=warming_m)
+print(on.month)
 
 # /home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/
-file = open("/home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/cron.txt", "w")
+file = open("cron.txt", "w")
 
 file.write(
-    str(on_minutes) + " " + str(on_hours) + " " + str(on_day) + " " + str(
-        on_month) + " * /usr/bin/python3 /home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/turnOn.py\n")
+    str(on.minute) + " " + str(on.hour) + " " + str(on.day) + " " + str(
+        on.month) + " * /usr/bin/python3 /home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/turnOn.py\n")
 file.write(
     str(off_minutes) + " " + str(off_hours) + " " + str(off_day) + " " + str(
         off_month) + " * /usr/bin/python3 /home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/turnOff.py\n")
