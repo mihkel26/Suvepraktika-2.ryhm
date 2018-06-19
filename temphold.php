490c1260dc31352a562edad419ceaeecf4408901
Mihkel Mägi <31948822+mihkel26@users.noreply.github.com>
2018-06-19 14:00:34 +0300

    Add files via upload

---
+++ temphold.php
@@ -0,0 +1,119 @@
+<?php
+$userNeeds = "";
+$minTempInput = null;
+$maxTempInput = null;
+$hourToTxtFile = "";
+$deviceState = "";
+
+
+$minTempInputError = "";
+$maxTempInputError = "";
+$notice = "";
+
+//kas vajutati sumbit nuppu
+if (isset($_POST["submitButton"])) {
+
+
+    if (isset($_POST["tempSelect"])) {
+        if (empty($_POST["tempSelect"])) {
+            $notice = "Nb, vaja on temperatuuri";
+        } else {
+            $tempInput = $_POST["tempSelect"];
+        }
+    }
+    //echo $_POST["tempSelect"];
+
+    if (!empty($minTempInputError) and !empty($maxTempInputError)) {
+        echo "All Values have to be chosen";
+    } else {
+        $myFile = fopen("userNeeds.txt", "w") or die ("Ei saa avada(file peab olema 'w' õigusega");
+        fwrite($myFile, "3\n\n\n\n\n" . $minTempInput . "\n" . $maxTempInput);
+        fclose($myFile);
+
+    }
+}
+
+
+$minTempSelectHTML = "";
+$minTempSelectHTML .= '<select name="minTempSelect">' . "\n";
+$minTempSelectHTML .= '<option value="" selected disabled>temp</option>' . "\n";
+for ($i = 3; $i < 25; $i++) {
+    if ($i == $minTempInput) {
+        $minTempSelectHTML .= '<option value="' . $i . '" selected>' . $i . '</option>' . "\n";
+    } else {
+        $minTempSelectHTML .= '<option value="' . $i . '">' . $i . '</option>' . " \n";
+    }
+
+}
+$minTempSelectHTML .= "</select> \n";
+
+$maxTempSelectHTML = "";
+$maxTempSelectHTML .= '<select name="maxTempSelect">' . "\n";
+$maxTempSelectHTML .= '<option value="" selected disabled>temp</option>' . "\n";
+for ($i = 3; $i < 25; $i++) {
+    if ($i == $maxTempInput) {
+        $maxTempSelectHTML .= '<option value="' . $i . '" selected>' . $i . '</option>' . "\n";
+    } else {
+        $maxTempSelectHTML .= '<option value="' . $i . '">' . $i . '</option>' . " \n";
+    }
+
+}
+$maxTempSelectHTML .= "</select> \n";
+
+
+//led
+$deviceFile = fopen("/home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/data.txt", "r") or die ("Ei saa avada");
+$deviceState = fread($deviceFile, filesize("/home/pi/suvepraktika/Suvepraktika-2.ryhm/main/python/src/data.txt"));
+fclose($deviceFile);
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <title> User input </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="design/design.css">
+</head>
+
+<style>
+</style>
+
+<body>
+<div id="write" style="text-align:center">
+    <h1> Sisesta oma soovid </h1>
+    <form method="POST">
+        <label>Sisesta minimaalne temperatuur </label>
+        <?php
+        echo $minTempSelectHTML;
+        ?>
+        <br><br>
+
+        <label> Sisesta maksimaalne temperatuur </label>
+        <?php
+        echo $maxTempSelectHTML;
+        ?>
+        <br><br>
+        <input name="submitButton" type="submit" value="Sisesta">
+    </form>
+
+</div>
+<br><br>
+<br><br>
+<div id="deviceState">
+    <label> device on/off?</label>
+    <?php
+    echo $deviceState;
+    ?>
+</div>
+
+<br><br>
+<br><br>
+<br><br>
+<div id="footer">
+    <a class="btn btn-small" href="http://greeny.cs.tlu.ee/~penjmart/Suvepraktika-2.ryhm/main.php">Tagasi pealehele</a>
+</div>
+
+
+</body>
+</html>
