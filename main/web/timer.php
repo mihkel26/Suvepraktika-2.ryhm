37c79fa07ce96817b2f7f45917f7547f70d286d7
mihkel26 <mihkel260@gmail.com>
2018-06-19 17:15:25 +0300

    timer php

--- main/web/timer.php
+++ main/web/timer.php
@@ -120,7 +120,7 @@ if (isset($_POST["submitButton"])) {
         echo "All Values have to be chosen";
     } else {
         $myFile = fopen("userNeeds.txt", "w") or die ("Ei saa avada(file peab olema 'w' õigusega");
-        fwrite($myFile, "2\n" . $timeInput . "\n".$dateDay ."." .$dateMonth"\n30\n" . $endTimeInput . "\n4\n8\n");
+        fwrite($myFile, "2\n" . $timeInput . "\n".$dateDay ."." . $dateMonth . "\n30\n" . $endTimeInput . "\n4\n8\n");
         fclose($myFile);
         echo "Korras";
 
