3f8e71f6d9b810066efb8aa3df12376f1553474c
mihkel26 <mihkel260@gmail.com>
2018-06-19 15:34:10 +0300

    web folder

---
+++ main/web/main.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<head>
+	<title> Tark Maja </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="design/design.css">
+</head>
+
+<body>
+	<div id="Header">
+	<h1> Targa Maja Lahendus</h1>
+	</div>
+		
+	
+	
+		<div id="userChoice">
+			<div id="shortcuts">
+				
+					
+					<a class="btn" href="userInterface.php">Kütteaeg kellaajaks</a>
+					
+					<a class="btn" href="timer.php">Vali ise ajavahemik</a>
+					
+					<a class="btn" href="holdtemp.php">Hoia temperatuuri</a>
+					
+			</div>		
+				
+		</div>
+		
+	
+</body>
+</html>
