6269b2b6ad3d3f287a7c178cbfa0018c3c464cb8
Joosep Penjam <penjmart@greeny.cs.tlu.ee>
2018-06-19 10:05:02 +0300

    User interface

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<head>
+	<title> Tark Maja </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="design/design.css">
+</head>
+
+<body>
+	<div id="Header">
+	<h1> Targa Maja Lahendus</h1>
+	</div>
+		
+	
+	
+		<div id="userChoice">
+			<div id="shortcuts">
+				
+					
+					<a class="btn" href="http://greeny.cs.tlu.ee/~penjmart/Suvepraktika-2.ryhm/userInterface.php">Kütteaeg kellaajaks</a>
+					
+					<a class="btn" href="">Kütteaeg perioodiks</a>
+					
+					<a class="btn" href="">Temperatuur</a>
+					
+			</div>		
+				
+		</div>
+		
+	
+</body>
+</html>
